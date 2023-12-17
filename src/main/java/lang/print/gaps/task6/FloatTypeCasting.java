c25001658d648eea65a47fdce7745e9096f01ed6
Constantine <77935386+velteren@users.noreply.github.com>
2023-12-17 20:11:41 +0300

    fix: errors

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -3,6 +3,6 @@ package lang.print.gaps.task6;
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
         int result = (int) numberToBeRounded;
-        System.out.println(result);
+        System.out.println(Math.round(numberToBeRounded));
     }
 }
